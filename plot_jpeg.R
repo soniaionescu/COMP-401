34504f09ceeff31f15aeca5618f7d5e3228ba49d
Sonia Ionescu <sonia-adeline@live.com>
2018-03-26 20:21:52 -0400

    Delete plot_jpeg.R

--- plot_jpeg.R
+++
@@ -1,8 +0,0 @@
-plot_jpeg <- function(path, add=FALSE){
-    require('jpeg')
-    jpg = readJPEG(path, native=T) # read the file
-    res = dim(jpg)[2:1] # get the resolution, [x, y]
-    if (!add) # initialize an empty plot area if add==FALSE
-        plot(1,1,xlim=c(1,res[1]),ylim=c(1,res[2]),asp=1,type='n',xaxs='i',yaxs='i',xaxt='n',yaxt='n',xlab='',ylab='',bty='n')
-    rasterImage(jpg,1,1,res[1],res[2])
-}
