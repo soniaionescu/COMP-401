27307547dcfa910a51cce8544a061294bd290aeb
Sonia Ionescu <sonia-adeline@live.com>
2018-03-26 20:30:05 -0400

    Update create_morphospace.R

--- create_morphospace.R
+++ create_morphospace.R
@@ -4,7 +4,7 @@ species_average <- function(filename){
     species <- unique(fishies$species) ## make a vector with the unique filenames
     atts <- colnames(fishies)[endsWith(colnames(fishies), "x")] ## make a vector with the unique attributes
     atts <- substr(atts, 1, nchar(atts) - 1) ## amend the vector with the unique attributes
-    ar <- array(dim = c(39, 2, length(species)), dimnames = list(atts, c("x", "y"), species)) ## make array and name dimensions
+    ar <- array(dim = c(28, 2, length(species)), dimnames = list(atts, c("x", "y"), species)) ## make array and name dimensions
     gpag <- overall_average(filename) ## scale every fish using GPA
     for(i in 1:length(species)){ ## for each species, find the average fish
         scaled_species <- gpag$coords[ , , fishies$species == species[i]]
@@ -17,21 +17,21 @@ overall_average <- function(filename){
     require(geomorph)
     fishies <- read.csv(filename)
     n_fish <- nrow(fishies)
-    ar <- array(dim = c(39, 2, n_fish))
+    ar <- array(dim = c(28, 2, n_fish))
     for(i in 1:n_fish){
-        ar[1:39, 1, i] <- as.numeric(fishies[i, endsWith(colnames(fishies), "x")])
-        ar[1:39, 2, i] <- as.numeric(fishies[i, endsWith(colnames(fishies), "y")])
+        ar[1:28, 1, i] <- as.numeric(fishies[i, endsWith(colnames(fishies), "x")])
+        ar[1:28, 2, i] <- as.numeric(fishies[i, endsWith(colnames(fishies), "y")])
     }
     return(gpagen(ar, print.progress = FALSE))
 }
-
 relative_warp <- function(array){
+    require(ggplot2)
     require(ggfortify)
     numberSpecies <- length(array[1,1,])
-    name_coords <- character(78)
+    name_coords <- character(56)
     xs <- paste0(dimnames(myArray)[[1]], "x")
     ys <- paste0(dimnames(myArray)[[1]], "y")
-    for(i in 1:78){
+    for(i in 1:56){
         if(i %% 2 == 1){
             name_coords[i] <- xs[ceiling(i/2)]
         }
@@ -39,11 +39,11 @@ relative_warp <- function(array){
             name_coords[i] <- ys[ceiling(i/2)]
         }
     }
-    cartesianCoords <- (matrix(0, ncol = 39*2, nrow = numberSpecies, 
+    cartesianCoords <- (matrix(0, ncol = 28*2, nrow = numberSpecies, 
                                dimnames = list(dimnames(array)[[3]], name_coords)))
     for(i in 1:numberSpecies){
         counter <- 1
-        for(j in 1:39){ ## put x value
+        for(j in 1:28){ ## put x value
             for(k in 1:2){ ## put y value
                 cartesianCoords[i, counter]<-array[j, k, i]
                 counter <- counter + 1
@@ -53,6 +53,8 @@ relative_warp <- function(array){
     
     df <- data.frame(cartesianCoords, species = dimnames(array)[[3]])
 
-    autoplot(prcomp(cartesianCoords), data = df, colour = 'species')
-    return(prcomp(cartesianCoords))
+    print(autoplot(prcomp(cartesianCoords), scale = FALSE, data = df, colour = 'species', main = "Morphospace"))
+    return(list(prcomp(cartesianCoords), df$species))
 }
+
+
