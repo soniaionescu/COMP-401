19f1c00ee17f85f7190a1865b4ac6d1c59c9683c
Sonia Ionescu <sonia-adeline@live.com>
2018-04-11 23:29:15 -0400

    Update morphospace_analysis.R

--- morphospace_analysis.R
+++ morphospace_analysis.R
@@ -1,18 +1,18 @@
 continuous_kernelDensity <- function(pca_object){
     require(MASS)
     pca <- pca_object[[1]]
-    xValues <-pca$x[, "PC1"]
-    yValues <- pca$x[,"PC2"]
-    kernelDensity <- kde2d(xValues, yValues, n=100, lims = c(-0.30, 0.30, -0.25, 0.20))
+    xValues <-pca[, "PC1"]
+    yValues <- pca[,"PC2"]
+    kernelDensity <- kde2d(xValues, yValues, n=100)
     filled.contour(kernelDensity, color.palette = 
                        colorRampPalette(c('white','blue','yellow','red','darkred'))) #gives colour and index
     title(main="Kernel density graph of morphospace", xlab="PC1", ylab="PC2")
 }
 contour_kernelDensity <- function(pca_object){
     pca <- pca_object[[1]]
-    numberSpecies <- nrow(pca$x)
-    xValues <-pca$x[, "PC1"]
-    yValues <- pca$x[,"PC2"]
+    numberSpecies <- nrow(pca)
+    xValues <-pca[, "PC1"]
+    yValues <- pca[,"PC2"]
     df <- data.frame(x=xValues,y=yValues,species=factor(pca_object[[2]]))
     commonTheme = list(labs(color="Density",fill="Density",
                             x="PC1", y="PC2"),
@@ -38,9 +38,9 @@ contour_kernelDensity <- function(pca_object){
 mountain_graph <- function(pca_object){
     require(MASS)
     pca <- pca_object[[1]]
-    xValues <-pca$x[, "PC1"]
-    yValues <- pca$x[,"PC2"]
-    kernelDensity <- kde2d(xValues, yValues, n=100, lims = c(-0.30, 0.30, -0.25, 0.20))
+    xValues <-pca[, "PC1"]
+    yValues <- pca[,"PC2"]
+    kernelDensity <- kde2d(xValues, yValues, n=100)
     persp(kernelDensity, phi= 5, theta = -30, d=10,
           ticktype="detailed", zlab='Density',
           ylab='PC2',xlab='PC1',main='Kernel density of morphospace') #phi=up down,theta=left right, d=dimension
@@ -50,12 +50,12 @@ mountain_graph <- function(pca_object){
 Voronoi_polygon <- function(pca_object){
     pca <- pca_object[[1]]
     require(deldir)
-    xValues <-pca$x[, "PC1"]
-    yValues <- pca$x[,"PC2"]
+    xValues <-pca[, "PC1"]
+    yValues <- pca[,"PC2"]
     df <- data.frame(x=xValues,y=yValues)
     graphV <- deldir(xValues, yValues)
     graphTiles <- tile.list(graphV)
-    numberSpecies <- nrow(pca$x)
+    numberSpecies <- nrow(pca)
     graphColors <- topo.colors(numberSpecies)
     plot(graphTiles, fillcol=graphColors, close=TRUE, ylab='PC2',xlab='PC1',main='Morphospace with Voronoi polygons')
     temp <- graphV$summary
